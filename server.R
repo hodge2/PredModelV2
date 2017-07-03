e8435d02d0fca125a29e489baec99feee29f825a
Miriam Hodge <un144@siap.local>
2017-07-03 10:38:26 +0900

    Basic Functions

---
+++ server.R
@@ -0,0 +1,29 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  person <- reactive({
+    tmp <-   skeleton
+    tmp[1, "hours"] <- input$hours
+    tmp[1, "agegrp_id"] <- input$agegrp_id
+    tmp[1, "occupation_id"] <- input$occupation_id
+    
+    return(tmp)
+  })
+  
+  
+  output$Prediction <- renderPrint({
+    
+    # prediction for the given values
+    y = suppressWarnings(predict(lmSTEP, newdata = person()))
+    return( y)
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,38 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+load("dimensions.rda")
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Model Display"),
+  # Sidebar with a slider input for number of hours
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("hours",
+                  "Hours of work:",
+                  min = 0,
+                  max = 60,
+                  value = 37),
+      selectInput("agegrp_id", 
+                  "Age group",
+                  choices = d_agegrp,
+                  selected = sample(d_agegrp, 1)),
+      selectInput("occupation_id", 
+                  "Current or most recent occupation",
+                  choices = d_occupation,
+                  selected = sample(d_occupation, 1))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      textOutput("Prediction")
+    )
+  )
+))
